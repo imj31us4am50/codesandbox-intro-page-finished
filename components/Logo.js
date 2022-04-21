97175fdce4ed994b8a23ae45c37c822125651ea1
Volosenco Andreea-Laura <91089388+imj31us4am50@users.noreply.github.com>
2022-04-21 11:01:00 +0300

    upd

--- components/Logo.js
+++ components/Logo.js
@@ -2,10 +2,8 @@ import LogoCSS from "/components/Logo.css";
 
 export default function Logo() {
   return (
-    <div class="row" style={LogoCSS}>
-      <a>
-        <img src="https://imj31us4am50.github.io/imgs/logo.png" />
-      </a>
+    <div class="row conaied" style={LogoCSS}>
+      <img src="https://imj31us4am50.github.io/imgs/logo.png" />
     </div>
   );
 }
